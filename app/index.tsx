98441caba43f636c0788c8b4828da86c4c2de42f
Opjasson <azmitheusos333@gmail.com>
2025-02-18 12:32:00 +0700

    activityIndicator Loading

--- app/index.tsx
+++ app/index.tsx
@@ -488,61 +488,122 @@
 
 // -------------- mengirimkan data dari halaman sebelumnya
 
-import * as React from "react";
-import { View, Text, Button } from "react-native";
+// import * as React from "react";
+// import { View, Text, Button } from "react-native";
+// import {
+//     NavigationContainer,
+//     NavigationIndependentTree,
+//     useNavigation,
+//     createStaticNavigation,
+// } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// function HomeScreen() {
+//     const navigation = useNavigation();
+//     return (
+//         <View
+//             style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//             <Text>Home Screen</Text>
+//             <Button
+//                 onPress={() =>
+//                     navigation.navigate("Detail", {
+//                         info1: "Saya dari halaman depan",
+//                     })
+//                 }
+//                 title="Goto Detail"
+//             />
+//         </View>
+//     );
+// }
+
+// function DetailScreen({ route }) {
+//     return (
+//         <View
+//             style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//             <Text>DetailScreen</Text>
+//             <Text>info1 : {route.params.info1}</Text>
+//         </View>
+//     );
+// }
+
+// const Stack = createNativeStackNavigator();
+
+// function RootStack() {
+//     return (
+//         <Stack.Navigator>
+//             <Stack.Screen name="Home" component={HomeScreen} />
+//             <Stack.Screen name="Detail" component={DetailScreen} />
+//         </Stack.Navigator>
+//     );
+// }
+
+// export default function App() {
+//     return (
+//         <NavigationIndependentTree>
+//             <NavigationContainer>
+//                 <RootStack />
+//             </NavigationContainer>
+//         </NavigationIndependentTree>
+//     );
+// }
+
+// --------------------- Component WebView
+
+// import React from 'react'
+// import { View, Text, Button } from "react-native";
+
+// import { WebView } from "react-native-webview"
+
+// export default function index() {
+//   return (
+//     <WebView
+//         // source={{ uri: 'https://youtube.com' }}
+//         source={{ html : ' <p>Hallo world</p>' }}
+//     />
+//   )
+// }
+
+// --------------- ActivityIndicator untuk menampilkan proses loading di react Native
+import React, { useState } from "react";
 import {
-    NavigationContainer,
-    NavigationIndependentTree,
-    useNavigation,
-    createStaticNavigation,
-} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+    Text,
+    View,
+    ActivityIndicator,
+    StyleSheet,
+    Button,
+} from "react-native";
 
-function HomeScreen() {
-    const navigation = useNavigation();
+const index = () => {
+    const [loading, setLoading] = useState(false);
+    const [titleButton, setTitleButton] = useState("Jalankan proses");
+
+    const handleButton = () => {
+        if (loading === false) {
+            setLoading(true);
+            setTitleButton("Stop Process")
+        } else {
+            setLoading(false);
+            setTitleButton("Jalankan Process");
+        }
+    };
     return (
-        <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Home Screen</Text>
-            <Button
-                onPress={() =>
-                    navigation.navigate("Detail", {
-                        info1: "Saya dari halaman depan",
-                    })
-                }
-                title="Goto Detail"
+        <View style={style.area}>
+            <ActivityIndicator
+                animating={loading}
+                color="#ff0000"
+                size={"large"}
             />
+            <Button onPress={handleButton} title={titleButton} />
         </View>
     );
-}
+};
 
-function DetailScreen({ route }) {
-    return (
-        <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>DetailScreen</Text>
-            <Text>info1 : {route.params.info1}</Text>
-        </View>
-    );
-}
+const style = StyleSheet.create({
+    area: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
 
-const Stack = createNativeStackNavigator();
-
-function RootStack() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Detail" component={DetailScreen} />
-        </Stack.Navigator>
-    );
-}
-
-export default function App() {
-    return (
-        <NavigationIndependentTree>
-            <NavigationContainer>
-                <RootStack />
-            </NavigationContainer>
-        </NavigationIndependentTree>
-    );
-}
+export default index;
